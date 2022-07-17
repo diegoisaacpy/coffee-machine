eed760cfa0e834df7984c34f1550bbc5b66c66bd
cheems <diegoisaacpy@gmail.com>
2022-07-17 12:24:15 -0400

    Menu prompt added

--- code.py
+++ code.py
@@ -23,3 +23,20 @@ MENU = {
         "cost": 3.0,
     }
 }
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+is_on = True
+
+while is_on:
+    choice = input("What do you want? (espresso/latte/capucchino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
